805892e1b1bb3253b4ee5d5c3791ff118f1ace67
Behnam <bbehnam3831@gmail.com>
2024-12-28 11:55:18 +0330

    added main in index.cpp

--- index.cpp
+++ index.cpp
@@ -2,3 +2,33 @@
 #include <array>
 #include <chrono>
 #include <cstdlib>
+
+void live_die();
+void print();
+bool should_live(bool current_status, int x_axis, int y_axis);
+
+const int SIZE{100};
+std::array<std::array<bool, SIZE>, SIZE> board;
+
+int main(int argc, char *argv[])
+{
+  std::srand(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
+  int row{0};
+  int column{0};
+
+  for (int i = 0; i < 1000; i++)
+  {
+    row = rand() % (SIZE - 1) + 1;
+    column = rand() % (SIZE - 1) + 1;
+
+    board[column][row] = true;
+  }
+
+  for (;;)
+  {
+    print();
+    live_die();
+  }
+
+  return 0;
+}
